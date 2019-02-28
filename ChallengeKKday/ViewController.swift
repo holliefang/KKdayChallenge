44b6b6be3081496b7addf56c552f60fca369979f
Hollie <fangsihan@sihande-macbook-air.local>
2019-02-28 10:57:46 +0800

    Initial Commit

---
+++ ChallengeKKday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChallengeKKday
+//
+//  Created by Sihan Fang on 2019/2/28.
+//  Copyright © 2019 Sihan Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
